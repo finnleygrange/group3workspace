896a7ee1c4e63f414aab9540b1054300e0e16a6a
finnleygrange <dev.finnleygrange@icloud.com>
2023-03-01 10:32:22 +0000

    XAMP Setup

---
+++ client-dashboard.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include("./includes/head.php") ?>
+</head>
+<body>
+    <h1>Performance Dashboard: Waterman Group</h1>
+    <?php include("./includes/nav.php") ?>
+</body>
+</html>
---
+++ includes/nav.php
@@ -0,0 +1,6 @@
+<nav>
+    <ul>
+        <li><a href="./user-dashboard.php">My Dashboard</a></li>
+        <li><a href="./client-dashboard.php">Waterman Group</a></li>
+    </ul>
+</nav>
