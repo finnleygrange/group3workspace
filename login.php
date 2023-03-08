9960489c48046393912cbbfb36d2f9ced8d909cf
DuyKhanhDo <116875186+DuyKhanhDo@users.noreply.github.com>
2023-03-08 10:09:14 +0000

    big update

--- login.php
+++ login.php
@@ -2,28 +2,26 @@
 <html lang="en" >
 <head>
   <meta charset="UTF-8">
-  <title>Register-WaterMan</title>
+  <title>Login-WaterMan</title>
   <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Rubik:400,700'><link rel="stylesheet" href="css\mobile.css">
 
 </head>
 <body>
 <!-- partial:index.partial.html -->
-<div class="register-form">
+<div class="login-form">
   <form>
-    <h1>Register</h1>
+    <h1>Login</h1>
     <div class="content">
-        <div class="input-field">
-            <input type="name" placeholder="Name" autocomplete="nope">
-          </div>
       <div class="input-field">
         <input type="email" placeholder="Email" autocomplete="nope">
       </div>
       <div class="input-field">
         <input type="password" placeholder="Password" autocomplete="new-password">
       </div>
-      <a href="login.html" class="link">Already Have Account?</a>
+      <a href="#" class="link">Forgot Your Password?</a>
     </div>
     <div class="action">
+      <button>Register</button>
       <button>Submit</button>
     </div>
   </form>
